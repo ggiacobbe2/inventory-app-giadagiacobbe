b367063b6c8271644d9864088dc860900e670007
Giada Giacobbe <ggiacobbe13@gmail.com>
2025-11-06 20:02:17 -0500

    added Item class

--- inclass15/lib/main.dart
+++ inclass15/lib/main.dart
@@ -26,6 +26,45 @@ class InventoryHomePage extends StatefulWidget {
   _InventoryHomePageState createState() => _InventoryHomePageState();
 }
 
+class Item {
+  final String? id;
+  final String name;
+  final int quantity;
+  final double price;
+  final String category;
+  final DateTime createdAt;
+
+  Item({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.price,
+    required this.category,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'quantity': quantity,
+      'price': price,
+      'category': category,
+      'createdAt': createdAt,
+    };
+  }
+
+  factory Item.fromMap(String id, Map<String, dynamic> map) {
+    return Item(
+      id: id,
+      name: map['name'] ?? '',
+      quantity: map['quantity'] ?? 0,
+      price: map['price']?.toDouble() ?? 0.0,
+      category: map['category'] ?? '',
+      createdAt: (map['createdAt'] as Timestamp).toDate(),
+    );
+  }
+}
+
 class _InventoryHomePageState extends State<InventoryHomePage> {
   // TODO: 1. Initialize Firestore & Create a Stream for items
   // TODO: 2. Build a ListView using a StreamBuilder to display items
