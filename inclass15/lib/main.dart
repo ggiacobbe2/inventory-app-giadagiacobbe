806dafde7ced0cbe6caa72afa6c43afebd04fd4d
Giada Giacobbe <ggiacobbe13@gmail.com>
2025-11-06 19:52:12 -0500

    initial commit

---
+++ inclass15/lib/main.dart
@@ -0,0 +1,57 @@
+// Import Flutter and Firestore packages
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  runApp(InventoryApp());
+}
+
+class InventoryApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Inventory Management App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: InventoryHomePage(title: 'Inventory Home Page'),
+    );
+  }
+}
+
+class InventoryHomePage extends StatefulWidget {
+  const InventoryHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _InventoryHomePageState createState() => _InventoryHomePageState();
+}
+
+class _InventoryHomePageState extends State<InventoryHomePage> {
+  // TODO: 1. Initialize Firestore & Create a Stream for items
+  // TODO: 2. Build a ListView using a StreamBuilder to display items
+  // TODO: 3. Implement Navigation to an "Add Item" screen
+  // TODO: 4. Implement one of the Delete methods (swipe or in-edit)
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Inventory Management System'),
+            // TODO: Replace this Text widget with your StreamBuilder & ListView
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // TODO: Navigate to the Add/Edit Item Form
+        },
+        tooltip: 'Add Item',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
